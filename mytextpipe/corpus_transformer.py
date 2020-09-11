d03c247cfa155b06b05c00b3b817a8877c6ae20a
osipenks <svosipenko@gmail.com>
2020-09-11 08:57:28 +0300

    Class documentation

--- mytextpipe/corpus_transformer.py
+++ mytextpipe/corpus_transformer.py
@@ -1,4 +1,34 @@
 class CorpusTransformer:
+    """
+    Pipeline for arbitrary corpora transformation and conversion
+
+    Use a fixed sequence of steps to process data in corpus.
+    Each step uses: transformation function, corpus to apply this function
+    and arbitrary parameters, if needed
+
+    Transformation function get dictionary as argument
+    with predefined keys 'doc_id' and 'corpus', and other user defined args
+
+    Usage example:
+
+    tr = CorpusTransformer(file_corp, html_corp, doc_ids)
+    tr.transform([
+                ('file_to_html', file_to_html, file_corp),
+                ('html_post_processing', html_corp_postpoces, html_corp)
+                ],
+
+                {'file_to_html': {'target': html_corp}},
+
+                 debug=True)
+
+    # Transformation function
+    def file_to_html(args_dict):
+        doc_id = args_dict['doc_id']
+        source = args_dict['corpus']
+        target = args_dict['target']
+        ...
+
+    """
 
     def __init__(self, source, target, ids=None):
         self.source = source
@@ -7,7 +37,7 @@ class CorpusTransformer:
 
     def transform(self, steps, step_args=None, ids=None, debug=False):
 
-        # For each step get parameters as dict {step_name: step_params}
+        # For each step get args as dict {step_name: step_params}
         func_args = dict()
         for step_name, step_func, corp in steps:
             init_args = {'doc_id': '', 'corpus': corp}
@@ -15,6 +45,7 @@ class CorpusTransformer:
             if step_args:
                 func_args[step_name] = step_args[step_name] if step_name in step_args else init_args
 
+        # Iterate through steps running step function with args
         doc_ids = ids if ids else self.ids
         for doc_id in doc_ids:
             prev_step = ''
